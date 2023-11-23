41818086b1ade3f4565724931b06d4c40920fb77
MateuszSlusarczyk <126509313+MateuszSlusarczyk@users.noreply.github.com>
2023-11-23 11:06:58 +0100

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
-import { connectMongoDB } from "@/lib/mongodb";
-import User from "@/models/user";
-import NextAuth from "next-auth/next";
+
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
+import { authenticateUser } from "./authenticateUser";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -12,44 +10,8 @@ export const authOptions: NextAuthOptions = {
       credentials: {},
 
       async authorize(credentials: any) {
-        const { email, password } = credentials;
-
-        try {
-          await connectMongoDB();
-          const currentDate = new Date();
-          const hashedPassword = await bcrypt.hash(password, 10);
-        
-
-         
-          let user = await User.findOneAndUpdate(
-              { email },
-              {
-                $set: {
-                  email,
-                  password: hashedPassword,
-                  OstatniaAktywnosc: currentDate,
-                  powiadomieniaWyslane: 0,
-                }
-              },
-              { upsert: false }
-            );
-          
-
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-
-          if (!passwordsMatch) {
-            alert("Złe dane logowania");
-            return;
-          }
-
-          user.OstatniaAktywnosc = currentDate;
-          await user.save()
-          .then(console.log(user))
-
-          return user;
-        } catch (error) {
-          console.log("Error: ", error);
-        }
+        const user = await authenticateUser(credentials);
+        return user;
       },
     }),
   ],
@@ -61,7 +23,3 @@ export const authOptions: NextAuthOptions = {
     signIn: "/",
   },
 };
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
