cec4fdd490958569dcac5478a74bb63950273f57
HubertCiura <147997409+HubertCiura@users.noreply.github.com>
2023-11-26 19:23:39 +0100

    Add files via upload

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,70 +5,51 @@ import Logo from 'public/Logo.webp';
 import { signIn } from "next-auth/react";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Link from "next/link";
-import { useSpring, animated } from '@react-spring/web';
 
+import Link from "next/link";
 const StartPage = ()=>{
   window.location.href = "/";
 }
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [loginSuccess, setLoginSuccess] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Nowa zmienna stanu
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const router = useRouter();
-
-  // Ustawienie animacji
-  const fadeIn = useSpring({
-    opacity: loginSuccess ? 1 : 0,
-    from: { opacity: 0 },
-  });
-
   const handleSignupClick = () => {
-    router.push('/rejestracja');
+      router.push("/rejestracja"); // Przekierowuje na stronę rejestracji po kliknięciu przycisku "Signup"
+  return 1;  
   };
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    try {
-      const res: any = await signIn('credentials', {
-        email,
-        password,
-        redirect: false,
-      });
-
-      if (res.error) {
-        alert('Invalid Credentials');
-        return;
-      } else {
-        const form = e.target;
-        form.reset();
-        setLoginSuccess(true);
-        setIsAuthenticated(true); // Ustawienie, że użytkownik jest zalogowany
-
-        // Po udanym zalogowaniu przekieruj użytkownika
-        setTimeout(() => {
-          router.push('/');
-        }, 2000); // Przekieruj po 2 sekundach
+  const handleSubmit = async (e:any) => {
+      e.preventDefault();
+  
+      try {
+        const res:any = await signIn("credentials", {
+          email,
+          password,
+          redirect: false,
+        });
+  
+        if (res.error ) {
+          alert("Invalid Credentials");
+          return;
+        }
+        else{const form = e.target;
+          form.reset();
+          router.push("/account");}
+  
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
 
   return (
-<div className="h-full w-full flex flex-row p-5">
+    <div className="h-full w-full flex flex-row p-5">
       <div className="h-full w-1/2 flex flex-col items-center justify-center">
-        <animated.div style={fadeIn}>
-          {isAuthenticated ? (
-            <div className="p-6">Witamy na serwisie!</div>
-          ) : (
-            <div className="p-6">Zaloguj się</div>
-          )}
-        </animated.div>
+        <Image src={Logo} alt="Logo" className="w-3/12" onClick={StartPage}/>
+        <div className="p-6">Zaloguj się</div>
         <form className="w-1/2 p-5" onSubmit={handleSubmit}>
           <div className="p-5 mb-2 border-2">
             <div className="border-l-three border-l-4 pl-2">
